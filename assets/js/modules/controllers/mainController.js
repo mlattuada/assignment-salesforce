64a4841646a2c3bd5121cc0f54b980fe9cd085de
Miguel Lattuada <Miguel Lattuada>
2016-05-17 15:48:02 -0300

    unused scope variables

--- assets/js/modules/controllers/mainController.js
+++ assets/js/modules/controllers/mainController.js
@@ -1,8 +1,7 @@
 angular.module('Controllers')
     .controller('MainController', ['$scope', '$location', 'ContactService', 'AuthService', function ($scope, $location, ContactService, AuthService) {
     	
-        $scope.message = '';
-        $scope.messageType = '';
+        
         
     	$scope.contacts = [];
 
